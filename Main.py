53019bcfc76545347b492d02583e511a9f827e31
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-01-29 16:04:20 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pyshorteners # Importing the pyshortener module for shortening urls.
 
-a = input("Enter the Link:") # Taking the link as input from user.
+a = input("Enter the Link: ") # Taking the link as input from user.
 b = pyshorteners.Shortener() # Defining the function.
 c = b.tinyurl.short(a) # Shortening the link.
+print('') # For looks only...
 print(c) # Printing the Shortened link.
